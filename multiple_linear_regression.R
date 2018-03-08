d537ffc2969b41a0fa3eebaa048374c0a7edc99e
chandhrrasekar <5canvin108@gmail.com>
2018-03-08 18:34:19 +0530

    first commit

---
+++ multiple_linear_regression.R
@@ -0,0 +1,51 @@
+# Multiple Linear Regression
+install.packages("scatterplot3d")
+install.packages("MASS")
+install.packages("rgl")
+install.packages("Rcmdr")
+
+# Importing the dataset
+dataset = read.csv('50_Startups.csv')
+
+# Encoding categorical data
+dataset$State = factor(dataset$State,
+                       levels = c('New York', 'California', 'Florida'),
+                       labels = c(1, 2, 3))
+
+# Splitting the dataset into the Training set and Test set
+# install.packages('caTools')
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Profit, SplitRatio = 0.8)
+View(split)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
+
+# Feature Scaling
+# training_set = scale(training_set)
+# test_set = scale(test_set)
+
+# Fitting Multiple Linear Regression to the Training set
+regressor = lm(formula = Profit ~ .,
+               data = training_set)
+summary(regressor)
+# Predicting the Test set results
+y_pred = predict(regressor, newdata = test_set)
+y_pred
+# Building the optimal model using Backward Elimination
+regressor = lm(formula = Profit ~ R.D.Spend + Administration + Marketing.Spend + State,
+               data = dataset)
+summary(regressor)
+# Optional Step: Remove State2 only (as opposed to removing State directly)
+# regressor = lm(formula = Profit ~ R.D.Spend + Administration + Marketing.Spend + factor(State, exclude = 2),
+#                data = dataset)
+# summary(regressor)
+regressor = lm(formula = Profit ~ R.D.Spend + Administration + Marketing.Spend,
+               data = dataset)
+summary(regressor)
+regressor = lm(formula = Profit ~ R.D.Spend + Marketing.Spend,
+               data = dataset)
+summary(regressor)
+regressor = lm(formula = Profit ~ R.D.Spend,
+               data = dataset)
+summary(regressor)
